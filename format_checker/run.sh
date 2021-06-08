9a6f1a6049742a03614dd6974a29ffa1a4ae96ee
Jere <jeremiasparladorio@gmail.com>
2021-06-08 19:02:29 -0300

    Update run.sh

--- format_checker/run.sh
+++ format_checker/run.sh
@@ -1,9 +1,7 @@
 # Usage: bash format_checker/run.sh
 
-path=$(pwd)/env/bin/python3.9
-$path -m pip install --upgrade pip
-python3 -m venv env
+python3.9 -m venv env
 source env/bin/activate
-pip install -r format_checker/requirements.txt
-python3 format_checker/main.py 
+pip3 install -r format_checker/requirements.txt
+python3.9 format_checker/main.py 
 deactivate
